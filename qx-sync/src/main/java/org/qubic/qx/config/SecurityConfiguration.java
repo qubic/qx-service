ff2328a088235505a32e548fa30914fba66e5aa7
mio <22691017-qubicj@users.noreply.gitlab.com>
2024-10-11 11:50:36 +0000

    Add security config
    * Mainly to hide metrics endpoint

---
+++ qx-sync/src/main/java/org/qubic/qx/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package org.qubic.qx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // not needed atm, but it would work (except test)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/login", "/v1/qx/**").permitAll() // accessible for all
+                        .pathMatchers("/actuator/health").permitAll() // for health monitoring
+                        .anyExchange().authenticated() // other urls
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults()) // login form
+                .build();
+    }
+
+}
