efca392581ae19d862e4aa21193c139ac70ee5b6
mio <22691017-qubicj@users.noreply.gitlab.com>
2024-10-18 10:09:55 +0000

    Enable request logging for debug purposes

---
+++ qx-api/src/main/java/org/qubic/qx/api/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package org.qubic.qx.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+
+}
