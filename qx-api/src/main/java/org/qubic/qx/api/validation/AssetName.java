3a1f2e60e15d8ae7a8cf27c35d1efdaa43a1fafa
mio <180114449+qubicmio@users.noreply.github.com>
2025-01-11 18:18:13 +0000

    Fix asset name validation
    * Allow numbers.
    * There are probably more letters that should be allowed. (7-bit ascii)

--- qx-api/src/main/java/org/qubic/qx/api/validation/AssetName.java
+++ qx-api/src/main/java/org/qubic/qx/api/validation/AssetName.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 
 import java.lang.annotation.*;
 
-@Pattern(regexp = "^[A-Z]{1,7}$")
+@Pattern(regexp = "^[A-Z0-9]{1,7}$")
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
